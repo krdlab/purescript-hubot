2ad5e578033acd0cbaadf924a23abcf904f92952
krdlab <krdlab@gmail.com>
2016-05-05 02:16:36 +0900

    Add example code

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -1,9 +1,18 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, return, unit, ($), bind, (++))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Hubot (Robot, respond, match, send)
+
+hubot :: forall e. Robot -> Eff e Unit
+hubot robot = do
+    respond robot {pattern: "test (\\w+)$", flag: "i"} $ \res -> do
+        send res "ok!"
+        m <- match res 1
+        log $ "debug: " ++ m
+    return unit
+
+main :: forall e. Eff e Unit
+main = return unit
